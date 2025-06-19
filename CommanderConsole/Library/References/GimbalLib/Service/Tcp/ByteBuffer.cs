04b028bc2d203bccd69a56ea86657e058d38c4b0
ychans37 <ychans37@gmail.com>
2025-06-19 13:30:37 +0900

    Move library into console and update namespaces

---
+++ CommanderConsole/Library/References/GimbalLib/Service/Tcp/ByteBuffer.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommanderConsole.Service.Tcp
+{
+    public class ByteBuffer
+    {
+        private const int BUFFER_COUNT = 1024 * 1024; //1MB
+        private byte[] _Buffer = new byte[BUFFER_COUNT];
+        private int _Count = 0;
+        private object _lock = new object();
+
+        private int ByteSearch(byte[] buffer, byte[] pattern, bool withPattern = false)
+        {
+            int pos = -1;
+
+            int length = buffer.Length;
+            int start = 0;
+            bool matched;
+
+            if (length > 0 && pattern.Length > 0 && start <= (length - pattern.Length) && length >= pattern.Length)
+            {
+                for (int i = start; i <= length - pattern.Length; i++)
+                {
+                    if (buffer[i] == pattern[0])
+                    {
+                        if (length > 1)
+                        {
+                            matched = true;
+                            for (int y = 1; y <= pattern.Length - 1; y++)
+                            {
+                                if (buffer[i + y] != pattern[y])
+                                {
+                                    matched = false;
+                                    break;
+                                }
+                            }
+
+                            if (matched)
+                            {
+                                pos = i;
+
+                                if (withPattern)
+                                {
+                                    pos += pattern.Length;
+                                }
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            pos = i;
+
+                            if (withPattern)
+                            {
+                                pos += pattern.Length;
+                            }
+
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return pos;
+        }
+
+        public int GetLength()
+        {
+            return _Count;
+        }
+
+        public int PatternSearch(byte[] pattern, bool withPattern)
+        {
+            return ByteSearch(_Buffer, pattern, withPattern);
+        }
+
+        public void PutByteBlock(byte[] bytes, int length)
+        {
+            lock (_lock)
+            {
+                Buffer.BlockCopy(bytes, 0, _Buffer, _Count, length);
+                _Count += length;
+            }
+        }
+
+        public bool PopByteBlock(ref byte[] bytes, int length)
+        {
+            bool copyComplete = false;
+
+            lock (_lock)
+            {
+                if (length <= _Count)
+                {
+                    //정해진 양만큼 복사 한다.
+                    Buffer.BlockCopy(_Buffer, 0, bytes, 0, length);
+
+                    //남은 데이터를 버퍼의 첫 부분으로 이동 한다.
+                    //이부분에 대해서 부하가 걸리기 때문에 추후 이 로직을 하지 않을 수 있도록 조정 한다.
+                    Buffer.BlockCopy(_Buffer, length, _Buffer, 0, _Count - length);
+
+                    //뒷 부분을 클리어 한다.
+                    Array.Clear(_Buffer, _Count - length, length);
+
+                    _Count -= length;
+
+                    copyComplete = true;
+                }
+            }
+
+            return copyComplete;
+        }
+
+        /*
+        public bool PopByteBlock(ref byte[] bytes, int pos, int length)
+        {
+            bool copyComplete = false;
+
+            lock (_lock)
+            {
+                if (length <= _Count && pos >= length)
+                {
+                    //정해진 양만큼 복사 한다.
+                    Buffer.BlockCopy(_Buffer, pos - length, bytes, 0, length);
+
+                    //남은 데이터를 버퍼의 첫 부분으로 이동 한다.
+                    //이부분에 대해서 부하가 걸리기 때문에 추후 이 로직을 하지 않을 수 있도록 조정 한다.
+                    Buffer.BlockCopy(_Buffer, pos, _Buffer, pos - length, _Count - pos);
+
+                    _Count -= length;
+
+                    //뒷 부분을 클리어 한다.
+                    Array.Clear(_Buffer, _Count, length);
+
+                    copyComplete = true;
+                }
+            }
+
+            return copyComplete;
+        }*/
+
+        public bool PopByteBlock(ref byte[] bytes, int pos, int length)
+        {
+            bool copyComplete = false;
+
+            lock (_lock)
+            {
+                //if (length <= _Count && pos >= length)
+                if (_Count - pos >= length)
+                {
+                    //정해진 양만큼 복사 한다.
+                    Buffer.BlockCopy(_Buffer, pos, bytes, 0, length);
+
+                    //남은 데이터를 버퍼의 첫 부분으로 이동 한다.
+                    //이부분에 대해서 부하가 걸리기 때문에 추후 이 로직을 하지 않을 수 있도록 조정 한다.
+                    Buffer.BlockCopy(_Buffer, pos + length, _Buffer, pos, _Count - length);
+
+                    _Count -= length;
+
+                    //뒷 부분을 클리어 한다.
+                    //??????
+                    Array.Clear(_Buffer, _Count, length);
+
+                    copyComplete = true;
+                }
+            }
+
+            return copyComplete;
+        }
+
+        public void Clear()
+        {
+            lock (_lock)
+            {
+                Array.Clear(_Buffer, 0, BUFFER_COUNT);
+                _Count = 0;
+            }
+        }
+    }
+}
---
+++ CommanderConsole/Library/References/GimbalLib/Service/Tcp/TcpClientHost.cs
@@ -0,0 +1,175 @@
+﻿using CommanderConsole.Extensions;
+using CommanderConsole.Service.Tcp;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommanderConsole.Service.Tcp
+{
+    public class TcpClientHost
+    {
+
+        public enum HostEventEnum : uint
+        {
+            Unknown = 0,
+            Trace,
+            ConnectionOk,
+            Disconnected,
+            Receive
+        };
+
+        public class HostEventData
+        {
+            public HostEventEnum eventEnum { get; set; }
+            public string eventMessage { get; set; }
+        }
+
+
+        [System.Serializable]
+        public delegate bool HostEventDelegate(int code, string message);
+
+        public string TcpServerAddress = "127.0.0.1";
+        public ushort TcpServerPortNumber = 9000;
+        public bool Connected = false;
+        public string PacketDelimiter = "\r\n";
+        //public HostEventDelegate OnHostEventHandler;
+
+        private StandardClient mStandardClient;
+        private Thread mClientThread;
+        private NetworkStream mConnectedNetworkStream;
+        private byte[] mPacketDelimiter;
+        ConcurrentQueue<HostEventData> mEventQueue = new System.Collections.Concurrent.ConcurrentQueue<HostEventData>();
+
+
+        public void Awake()
+        {
+            TcpServerAddress = "192.168.10.223";
+            TcpServerPortNumber = (ushort)4001;
+
+            string tmpPacketDelimiter = PacketDelimiter;
+            tmpPacketDelimiter = tmpPacketDelimiter.Replace("\\r", "\r").Replace("\\n", "\n");
+
+            mPacketDelimiter = Encoding.Default.GetBytes(tmpPacketDelimiter);
+        }
+        // Start is called before the first frame update
+        public void Start()
+        {
+            mStandardClient = new StandardClient(this.OnMessage, this.ConnectionHandler, TcpServerAddress, TcpServerPortNumber); //Uses default host and port and timeouts
+            mClientThread = new Thread(this.mStandardClient.Run);
+
+            mClientThread.Start();
+        }
+
+        // Update is called once per frame
+
+        public void Dispose()
+        {
+            //mStandardClient.
+            if (mConnectedNetworkStream != null)
+            {
+                mConnectedNetworkStream.Close();
+            }
+
+            if (mStandardClient != null)
+            {
+                mStandardClient.ExitSignal = true;
+            }
+
+            if (mClientThread != null)
+            {
+                mClientThread.Join();
+            }
+        }
+
+        protected virtual void ConnectionHandler(NetworkStream connectedAutoDisposedNetStream)
+        {
+            if (!connectedAutoDisposedNetStream.CanRead && !connectedAutoDisposedNetStream.CanWrite)
+            {
+                return; //We need to be able to read and write
+            }
+
+            mConnectedNetworkStream = connectedAutoDisposedNetStream;
+
+            Connected = true;
+            mEventQueue.Enqueue(new HostEventData()
+            {
+                eventEnum = HostEventEnum.ConnectionOk,
+                eventMessage = "Connection Ok"
+            });
+
+            var writer = new StreamWriter(connectedAutoDisposedNetStream) { AutoFlush = true };
+            var reader = new StreamReader(connectedAutoDisposedNetStream);
+
+            ByteBuffer BB = new ByteBuffer();
+            byte[] recvBuffer = new byte[4096];
+
+            while (!this.mStandardClient.ExitSignal) //Tight network message-loop (optional)
+            {
+                try
+                {
+                    int length = connectedAutoDisposedNetStream.Read(recvBuffer, 0, recvBuffer.Length);
+
+                    if (length <= 0)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine(length);
+                }
+                catch (IOException ex)
+                {
+                    _ = ex;
+                    //this.OnHostMessages.Invoke(HostEvent.Trace, ex.ToString());
+                    break;
+                }
+            }
+
+            Connected = false;
+
+            mConnectedNetworkStream = null;
+
+            mEventQueue.Enqueue(new HostEventData()
+            {
+                eventEnum = HostEventEnum.Disconnected,
+                eventMessage = "Disconnected"
+            });
+        }
+
+        public bool WriteMessage(string message)
+        {
+            byte[] msg = Encoding.Default.GetBytes(message + Encoding.Default.GetString(mPacketDelimiter));
+            if (mConnectedNetworkStream != null)
+            {
+                try
+                {
+                    mConnectedNetworkStream.Write(msg, 0, msg.Length);
+                    mConnectedNetworkStream.Flush();
+                }
+                catch (Exception ex)
+                {
+                    _ = ex;
+
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        protected virtual void OnMessage(string message)
+        {
+            mEventQueue.Enqueue(new HostEventData()
+            {
+                eventEnum = HostEventEnum.Trace,
+                eventMessage = message
+            });
+        }
+    }
+}
